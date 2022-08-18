c8bd033505538eaffe6830bf1506a2ac38793c88
etosolo322 <etosolo322@yandex.ru>
2022-08-18 21:26:27 +0300

    multiply and divide are ready

--- src/module/calculation.js
+++ src/module/calculation.js
@@ -3,30 +3,41 @@ const betweenLeft = (string) =>{
     return string.slice(buf+1)
 }
 const betweenRight = (string) =>{
-    let buf = Math.max.apply(null,[string.indexOf('-')===0?-1:string.indexOf('-'),string.indexOf('+'),string.indexOf('/'), string.indexOf('×')]  )
+    let buf = Math.min.apply(null,[string.indexOf('-'),string.indexOf('+'),string.indexOf('/'), string.indexOf('×')].map(el=>el<0?900:el))
+  
     
     return buf>0?string.slice(0,buf):string
 }
 const multiplyOrDivide = (string, ind) =>{    
     let left = betweenLeft(string.slice(0,ind))
     let right = betweenRight(string.slice(ind+1))
-   console.log(left)
-   console.log(right)
+    if (string[ind]==='/'){
+        return string.replace(left+'/'+right, (left/right).toFixed(3))
+    } else{
+        return string.replace(left+'×'+right, (left*right).toFixed(3))
+    }
 }
 
-export const calculating = (response) =>{
-    let string = response[response.length-1]==='×' || response[response.length-1]==='+' || response[response.length-1]==='/' || response[response.length-1]==='-'?response.slice(0,response.length-1):response
-    let mulInd = string.indexOf('×')
-    let divideInd = string.indexOf('/')
-     if (mulInd>0 || divideInd>0){
-        if (mulInd>0 && divideInd>0){
-            multiplyOrDivide(string, divideInd>0?divideInd<mulInd?divideInd:mulInd:mulInd)
-        }
-        else if(mulInd>0){
-            multiplyOrDivide(string, mulInd)
-        }else{
-            multiplyOrDivide(string, divideInd)
+const multiplyAndDivideAll = (string)=>{
+    while (string.indexOf('×')!==-1 || string.indexOf('/')!==-1){        
+        let mulInd = string.indexOf('×')
+        let divideInd = string.indexOf('/')
+         if (mulInd>0 || divideInd>0){
+            if (mulInd>0 && divideInd>0){
+                string = multiplyOrDivide(string, divideInd<mulInd?divideInd:mulInd)
+            }
+            else if(mulInd>0){
+                string = multiplyOrDivide(string, mulInd)
+            }else{
+                string = multiplyOrDivide(string, divideInd)
+            }
         }
     }
+    return(string)
+}
+export const calculating = (response) =>{
+    let string = response[response.length-1]==='×' || response[response.length-1]==='+' || response[response.length-1]==='/' || response[response.length-1]==='-'?response.slice(0,response.length-1):response
+    string = multiplyAndDivideAll(string)
+    console.log(string)
 }
 
