09d6e44efdb3f52df07e3071e84da26d1219010b
etosolo322 <etosolo322@yandex.ru>
2022-08-19 01:34:59 +0300

    I'll go sleep

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -21,42 +21,42 @@ const App = ()=> {
   return (   
     calculation===false || calculation===""?
     <div className="calculator container">
-    <div className='calculator_title'>
-      <span>SUPER SMART CALCULATOR</span>
-      <span>Alexandr Gurski</span>
-    </div>
-    <div className='calculator_input' ref={inputs}>{input}</div>
-    <div className='calculator_keyboard'>
-      <span 
-        onClick={()=>
-          {
-          onCalc(inputs)
-          setCalc(!calc)
-          setInput('')
-          }} 
-          className='key-on'>
-          {!calc?'ON':'OFF'}
-      </span>
-      <span onClick={()=>setInput(input.slice(0,-1))} >←</span>
-      <span onClick={()=>setInput(reverseNumber(input))}>+/-</span> 
-      <span onClick={(e)=>calc?setInput(checkSymbal(input,e.target.innerHTML)):null}>×</span>         
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number'>1</span>
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number'>2</span>
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >3</span>
-      <span onClick={(e)=>calc?setInput(checkSymbal(input,e.target.innerHTML)):null}>/</span>
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >4</span>
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >5</span>
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >6</span>      
-      <span onClick={(e)=>calc?setInput(checkSymbal(input,e.target.innerHTML)):null}>-</span>
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >7</span>
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >8</span>
-      <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >9</span>
-      <span onClick={(e)=>calc?setInput(checkSymbal(input,e.target.innerHTML)):null}>+</span>
-      <span onClick={()=>calc?setInput(checkZero(input)):null} className='key-number' >0</span>
-      <span onClick={()=>calc?setInput(checkDoubleZero(input)):null} className='key-number' >00</span>
-      <span onClick={()=>calc?setInput(checkDoth(input)):null} className='key-number' >.</span>
-      <span onClick={()=>calc?setCalculation(calculating(input)):null}className='key-equal'>=</span>      
-    </div>
+      <div className='calculator_title'>
+        <span>SUPER SMART CALCULATOR</span>
+        <span>Alexandr Gurski</span>
+      </div>
+      <div className='calculator_input' ref={inputs}>{input}</div>
+      <div className='calculator_keyboard'>
+        <span 
+          onClick={()=>
+            {
+            onCalc(inputs)
+            setCalc(!calc)
+            setInput('')
+            }} 
+            className='key-on'>
+            {!calc?'ON':'OFF'}
+        </span>
+        <span onClick={()=>setInput(input.slice(0,-1))} >←</span>
+        <span onClick={()=>setInput(reverseNumber(input))}>+/-</span> 
+        <span onClick={(e)=>calc?setInput(checkSymbal(input,e.target.innerHTML)):null}>×</span>         
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number'>1</span>
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number'>2</span>
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >3</span>
+        <span onClick={(e)=>calc?setInput(checkSymbal(input,e.target.innerHTML)):null}>/</span>
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >4</span>
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >5</span>
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >6</span>      
+        <span onClick={(e)=>calc?setInput(checkSymbal(input,e.target.innerHTML)):null}>-</span>
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >7</span>
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >8</span>
+        <span onClick={(e)=>calc?setInput(input+e.target.innerHTML):null} className='key-number' >9</span>
+        <span onClick={(e)=>calc?setInput(checkSymbal(input,e.target.innerHTML)):null}>+</span>
+        <span onClick={()=>calc?setInput(checkZero(input)):null} className='key-number' >0</span>
+        <span onClick={()=>calc?setInput(checkDoubleZero(input)):null} className='key-number' >00</span>
+        <span onClick={()=>calc?setInput(checkDoth(input)):null} className='key-number' >.</span>
+        <span onClick={()=>calc?setCalculation(calculating(input)):null}className='key-equal'>=</span>      
+      </div>
    </div>
    :
    <div className='container hangman'>
@@ -64,7 +64,7 @@ const App = ()=> {
       </div>
       <div className='hangman_quiz'>
             <div className='hangman_quiz_input'>
-            { calculation.split('').map(sym=><span className='hangman_quiz_input_symbal'>{sym}</span>)}
+            { calculation.split('').map((sym,ind)=><span className='hangman_quiz_input_symbal' key={ind}>{sym}</span>)}
             </div>
             <div className='hangman_quiz_buttons'>
               <div className='hangman_quiz_buttons_container'>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/css/global.css'
-import Calculator from './components/calculator';
+import App from './components/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>
 );
 
